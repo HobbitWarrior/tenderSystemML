040522bf21a305da300311983f5ca54ceb0f69d4
sapirm523 <sapirm523@gmail.com>
2018-12-19 12:13:10 +0200

    updated a class

---
+++ materialDesignTesting/vectorFileHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace materialDesignTesting
+{
+    class vectorFileHandler
+    {
+
+
+
+public double[] WriteToFile(string path=@"C:\projTest\vector.txt",string output=@"C:\projTest\wtest.txt")
+{
+        //write to file
+     //string path = @"C:\projTest\vector.txt";
+     //string output = @"C:\projTest\wtest.txt";
+     string[] val = { "aa", "b", "c" };
+    double[] realval;
+     try
+     {
+        
+
+         StringBuilder builder = new StringBuilder();
+         for (int i = 0; i < val.Length; i++)
+             builder.AppendLine(val[i]);
+
+         File.WriteAllText(output, builder.ToString());
+         return
+     }
+     catch (Exception err)
+     {
+         Console.WriteLine(err.Message);
+     }
+
+
+
+}
+
+public double[] readVector(string path)
+        {
+            string[] text;
+            double[] realval;
+              try
+                 {
+                      if (!File.Exists(path))
+                         {
+                           Console.WriteLine("file not exist");
+                            //return ;
+                           }
+
+                        text = File.ReadAllLines(path);
+         for (int i = 0; i < text.Length; i++)
+         {
+             var temp = text[i].Replace("\t", "").Split(' ');
+             realval[i] =Int32( temp[i]);
+         }
+         return realval;
+     }
+     catch (Exception err)
+     {
+         Console.WriteLine(err.Message);
+     }
+
+
+        }
+    }
+}
