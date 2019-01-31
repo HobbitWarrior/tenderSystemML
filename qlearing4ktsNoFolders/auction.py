6b987a27008aa211c1a87d832001c4cc139f1b76
AlexZ <wolfazb@gmail.com>
2019-01-31 08:17:12 +0200

    Added simulateGame and averageInGames methods

--- qlearing4ktsNoFolders/auction.py
+++ qlearing4ktsNoFolders/auction.py
@@ -1,6 +1,7 @@
 import numpy as np
 from game import Game
-from __builtin__ import True
+import random
+from __builtin__ import True, False
 
 class Auction(Game):
     
@@ -18,10 +19,13 @@ class Auction(Game):
     
     def __init__(self,isFirst=False,N=100,maxMove=1000,opponentOutcome=None,K=2000):
         #initiate global fields
+        self.testSimulGame()
+        return 
         self.maxMove = maxMove
         self.EUser = np.zeros((2,self.maxMove))
         self.EOpponent = np.zeros((2,self.maxMove))
         self.reset()
+        
     @property
     def name(self):
         return "Auction"
@@ -40,9 +44,10 @@ class Auction(Game):
         self.step = 1
         
     """Implementation of a single round of a game, a single round consists of two moves, the player's move and the opponent's response,
-        the order or playing my vary. """    
+        the order of playing my vary. """    
     def play(self, action):
 		#Decided to pass
+        print("current action is {0}".format(action))
         if not action:
             self.passGame()
          #Check if we played maxMoves
@@ -56,7 +61,9 @@ class Auction(Game):
             #emulate opponents move,
             #if 1 then the opponent is still playing
             #if 0 then the opponents passed and hence the player won
-            if not np.random.choice(1,1):
+            opponentsMove=np.random.choice(1,1)
+            print(opponentsMove[0])
+            if not opponentsMove[0]:
                 self.winGame()
             
             
@@ -106,12 +113,13 @@ class Auction(Game):
             Expectations[step]."""
     def calculateExpecation(self):
         #TODO: to write the loss expectation
-        self.EUser[0,self.step] = ( self.userOutcome[self.step,0]*self.p[self.step] + (-self.step)*self.p[self.step] ) * ( 1 if self.step==1 else self.EUser[0,self.step-2] )
+        self.EUser[0,self.step] = ( self.userOutcome[0,self.step]*self.p[self.step] + (-self.step)*self.p[self.step] ) * ( 1 if self.step==1 else self.EUser[0,self.step-2] )
         return self.EUser[0,self.step]
         
     
     
     def calculateOutcome(self,isFirst=False,N=100,maxMove=1000,opponentOutcome=None,K=2000):
+        
         self.userOutcome = np.zeros((2,maxMove))
         
         self.q = np.arange(0.0001,1,0.0001)
@@ -130,9 +138,6 @@ class Auction(Game):
                     usersMove = np.random.uniform(0.0,1.0)
                     if(usersMove>self.q[iMove]):
                         #User won
-                        #save the current probability,
-                        #since it will be beneficial for the user to play this round
-                        self.p[iMove] = usersMove
                         #keep setting the entire outcome vector to the max,
                         #ie. simulate a win of the user in the entire rounds
                         #from this point on.
@@ -159,3 +164,67 @@ class Auction(Game):
                             self.userOutcome[1,j] = -j
                             #exit the current game :P jeez this shit is effing ridiculous
                             break
+                        
+                        
+                                       
+                        
+                        
+    def averageInGames(self,p,q, K, nGames):
+        X = np.zeros(nGames)
+        Y  = np.zeros(nGames)
+        n = np.zeros(nGames)
+        s1 = 0
+        s2 = 0 
+        s = 0
+        
+        for i in range(1,nGames):
+            X[i] , Y[i], n[i], r = self.simulateGame(p,q,K)     #simulate nGames, save players profits, and the reached round
+            s1 = s1 + X[i]                                      #sum of profits
+            s2 = s2 + Y[i]                                      
+            s = s + n[i]                                        #total number of played rounds
+        s1 = s1 / nGames                                        #Average earnings in nGames
+        s2 = s2 / nGames
+        s = s/nGames
+        return s1,s2,s
+    
+    
+                       
+    def simulateGame(self,p,q ,K):
+        i=0
+        endGame=False
+        
+        while not endGame:
+            i=i+1
+            r = random.random()
+            if r < p[i]:        #player plays
+                i=i+1
+                r = random.random() 
+                if  r < q[i]:   #Opponent plays too
+                    pass
+                else:           #opponents quits the game
+                    X = K-(i-1) #players profit
+                    Y = -(i-2)  #Opponents Profit
+                    return (X,Y, i , r)
+            else:               #player quits the game
+                 X = -(i-2)
+                 Y = K-(i-1)
+                 return (X,Y, i, r)    
+            if i > 1000*K:      #players reached his investment limit
+                X = -(i-2)
+                Y = K-(i-1)
+                return (X,Y,i,r)
+    
+    def testSimulGame(self):
+        K = 20
+        p = np.full(1000000,0.9)
+        
+        
+        q = np.full(1000000, 0.56)
+        for i in range(1,20):
+            X , Y , i ,r = self.simulateGame(p,q,K)
+            print(X,Y,i,r)
+        X,Y,N= self.averageInGames(p,q, K , 10000)
+        print(X,Y,N)
+             
+auction = Auction()
+           
