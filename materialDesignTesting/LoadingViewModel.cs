71315e7ee55e3348fcccba8da571b1caa9ef3170
AlexZ <wolfazb@gmail.com>
2019-02-04 15:47:22 +0200

    Added a loading window

---
+++ materialDesignTesting/LoadingViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace materialDesignTesting
+{
+
+    /// <summary>
+    ///  The ViewModel of the Loading UserControl
+    /// </summary>
+    class LoadingViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+     
+        private void RaisePropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
