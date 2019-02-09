37a59cee8ebd7cc0aacbbfd25e25e471788ff903
AlexZ <wolfazb@gmail.com>
2019-02-09 11:32:52 +0200

    uodated the GUI of the game settings

--- materialDesignTesting/MainWindow.xaml.cs
+++ materialDesignTesting/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace materialDesignTesting
             {
                 //note you can use the message queue from any thread, but just for the demo here we 
                 //need to get the message queue from the snackbar, so need to be on the dispatcher
-                MainSnackbar.MessageQueue.Enqueue("Welcome to Material Design In XAML Tookit");
+                MainSnackbar.MessageQueue.Enqueue("Welcome to an Application of game theory for participating in tenders");
             }, TaskScheduler.FromCurrentSynchronizationContext());
 
            // DataContext = new MainWindowViewModel(MainSnackbar.MessageQueue);
